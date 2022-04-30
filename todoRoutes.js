1e24c102ebf7c77b442a459a339cfe5ebd3c330b
Sachin <sachinlaloo@gmail.com>
2022-04-30 13:48:35 +0200

    Added a list of todos with an initial todo of "Do Some Work""

--- todoRoutes.js
+++ todoRoutes.js
@@ -2,6 +2,16 @@ let path = require('path');
 let express = require('express');
 let todoAPP = express.Router();
 
+let todoList = [
+
+    {
+        name: 'Do Some Work',
+        completed: false,
+        number: 1
+    },
+
+]
+
 todoAPP.get('/', function (req, res){
     res.send('Hello World!');
 });
