02eb3bd1dc2cca5bce1a362d9b863968964f54dd
Sachin <sachinlaloo@gmail.com>
2022-05-01 22:32:06 +0200

    Added post route to update a todo item

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 let path = require('path');
 let express = require('express');
+
 const { Console } = require('console');
 let app = express();
-let todoAPP = require('./todoRoutes');
 
 let bodyParser = require('body-parser');
+
+
+let todoAPP = require('./todoRoutes.js');
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
